ad91c77117bdc5aea0ca1d60de85e4053da19745
Claire Stranack <claire@stranack.co.uk>
2020-09-21 19:51:33 +0100

    designed webiste

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars');
+
+//---ROUTES---
+app.use(express.static('public'));
+
+
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs'
+}));
+
+
+//web application getting something from the server
+//req = request //res = response //=>'fat arrow' = function
+app.get('/', (req, res) => {
+    res.render('homePage', {layout: 'main'} );
+
+});
+
+
+//listening for requests on port 3000
+app.listen(3000,() => {
+    console.log(' Server listening on port 3000 :) ');
+});
